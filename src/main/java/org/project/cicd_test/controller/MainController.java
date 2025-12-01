aaebbcf80fe4330d338b8b0f3e3e27b078f52726
zionge2k <zion.geek.py@gmail.com>
2025-12-01 10:49:32 +0900

    test 6 - fix2

---
+++ src/main/java/org/project/cicd_test/controller/MainController.java
@@ -0,0 +1,14 @@
+package org.project.cicd_test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+    @GetMapping
+    public String main() {
+        return "Welcome to the CI/CD Test Application!";
+    }
+}
