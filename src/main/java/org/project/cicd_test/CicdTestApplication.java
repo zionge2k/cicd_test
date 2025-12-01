2320324bf88469f548469e92352f2120beb8506d
zionge2k <zion.geek.py@gmail.com>
2025-12-01 09:25:21 +0900

    first commit

---
+++ src/main/java/org/project/cicd_test/CicdTestApplication.java
@@ -0,0 +1,13 @@
+package org.project.cicd_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/cicd_test/CicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.cicd_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
